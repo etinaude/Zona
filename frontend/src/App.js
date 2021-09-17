190060378d3eda0685f25c497259e25f4350649b
Etienne <software@etinaude.dev>
2021-09-18 01:47:02 +1200

    about page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,15 +2,19 @@ import './App.css';
 import React from 'react';
 import Table from './components/Table';
 import Graph from './components/Chart';
+import Nav from './components/Nav';
+import About from "./pages/About"
 
 
 
 function App() {
 
   return (
-    <div className="App">
-      <Table></Table>
-      <Graph></Graph>
+    <div>
+      <Nav></Nav>
+      <About></About>
+      {/* <Table></Table> */}
+      {/* <Graph></Graph> */}
     </div>
   );
 }
